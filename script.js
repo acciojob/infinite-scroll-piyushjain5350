e49beb7454598bde01116a83a4828182b067fe52
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-10-06 22:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
+const scrollbox = document.getElementById("infi-list");
 
+let y =1;
+function createitem(){
+	
+    for(let i = 0; i<10 ; i++){
+        const createelement = document.createElement("li");
+        createelement.innerText= `Item ${y}`
+		scrollbox.appendChild(createelement);
+		y++;
+	}
+}
+
+
+function addmoreelements(){
+    const totolscroll = scrollbox.scrollTop;
+    const maxscroll = scrollbox.scrollHeight - scrollbox.clientHeight;
+    const threshold = maxscroll-totolscroll;
+    if(threshold<=5){
+        for(let i = 0; i<2; i++){
+            createitem();
+        }
+    }
+}
+
+createitem();
+
+scrollbox.addEventListener("scroll" , addmoreelements)
